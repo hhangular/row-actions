dce09ec1a67a5d39f19db242225e7f94dc009acc
hhfrancois <francois.achache@gmail.com>
2024-03-06 11:06:32 +0100

    Fix animation bug in row-actions component

--- src/lib/row-actions.component.ts
+++ src/lib/row-actions.component.ts
@@ -1,4 +1,4 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { ConnectedPosition, OverlayModule } from '@angular/cdk/overlay';
 import { AsyncPipe, NgStyle } from '@angular/common';
 import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, HostBinding, Input } from '@angular/core';
@@ -7,17 +7,18 @@ import { BehaviorSubject, Observable, combineLatestWith, debounceTime, map } fro
 
 
 @Component({
-    selector: 'row-actions',
-    standalone: true,
-    template: `
+  selector: 'row-actions',
+  standalone: true,
+  template: `
         <span class="actions-trigger" cdkOverlayOrigin #trigger="cdkOverlayOrigin"></span>
         <ng-template cdkConnectedOverlay [cdkConnectedOverlayPositions]="overlayPositions" [cdkConnectedOverlayOrigin]="trigger" [cdkConnectedOverlayOpen]="!!(show$ | async)">
-            <mat-toolbar [ngStyle]="{height: heightToolbar, minHeight: heightToolbar, maxHeight: heightToolbar}" [color]="color" (mouseenter)="hover$.next(true)" (mouseleave)="hover$.next(false)" @toolbarAppear>
-                <ng-content></ng-content>
-            </mat-toolbar>
+          <mat-toolbar [ngStyle]="{height: heightToolbar, minHeight: heightToolbar, maxHeight: heightToolbar}" [color]="color"
+            (mouseenter)="hover$.next(true)" (mouseleave)="hover$.next(false)" [@expandFromRight]="animatedFrom" [@expandFromLeft]="animatedFrom">
+              <ng-content></ng-content>
+          </mat-toolbar>
         </ng-template>
     `,
-    styles: [`
+  styles: [`
         .actions-trigger {
             display: flex;
             flex-grow: 1;
@@ -29,75 +30,102 @@ import { BehaviorSubject, Observable, combineLatestWith, debounceTime, map } fro
             display: flex;
         }
    `],
-   imports: [
+  imports: [
     AsyncPipe,
     NgStyle,
     MatToolbarModule,
     OverlayModule,
-   ],
-    animations: [
-        trigger('toolbarAppear', [
-            transition(':enter', [
-                style({ opacity: 0 }), animate(300)
-            ]),
-            transition(':leave', [
-                animate(300, style({ opacity: 0 }))
-            ])
-        ])
-    ],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  ],
+  animations: [
+    trigger('expandFromRight', [
+      state('void', style({ transform: 'scaleX(0)', transformOrigin: 'right center' })),
+      state('right', style({ transform: 'scaleX(1)', transformOrigin: 'right center' })),
+      transition('void => right', animate('300ms ease-out')),
+      transition('right => void', animate('300ms ease-in'))
+    ]),
+    trigger('expandFromLeft', [
+      state('void', style({ transform: 'scaleX(0)', transformOrigin: 'left center' })),
+      state('left', style({ transform: 'scaleX(1)', transformOrigin: 'left center' })),
+      transition('void => left', animate('300ms ease-out')),
+      transition('left => void', animate('300ms ease-in'))
+    ])
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RowActionComponent implements AfterViewInit {
 
-    overlayPositions: ConnectedPosition[] = [{ originY: 'top', originX: 'end', overlayY: 'top', overlayX: 'end' }];
+  overlayPositions: ConnectedPosition[] = [{ originY: 'top', originX: 'end', overlayY: 'top', overlayX: 'end' }];
 
-    open$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    hover$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    show$: Observable<boolean>;
+  open$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  hover$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  show$: Observable<boolean>;
 
-    heightToolbar: string = '48px';
+  heightToolbar: string = '48px';
 
-    constructor(
-        private el: ElementRef,
-    ) {
-        this.show$ = this.open$.pipe(
-            combineLatestWith(this.hover$),
-            debounceTime(100),
-            map(([open, hover]: [boolean, boolean]) => open || hover)
-        );
+  position: 'left' | 'right' = 'right';
 
+  @Input()
+  animationDisabled: boolean = false;
+
+  animatedFrom: 'left' | 'right' | null = null;
+
+  constructor(
+    private el: ElementRef,
+  ) {
+    this.show$ = this.open$.pipe(
+      combineLatestWith(this.hover$),
+      debounceTime(1),
+      map(([open, hover]: [boolean, boolean]) => open || hover)
+    );
+  }
+  ngAfterViewInit(): void {
+    const parentElement = this.el.nativeElement.parentElement;
+    const parentStyle = getComputedStyle(parentElement);
+    // parentElement.childNodes[this.el.nativeElement.parentElement.childNodes.length - 1] === this.el.nativeElement ? 'right' : 'left';
+    this.position = parentElement.childNodes[0] === this.el.nativeElement ? 'left' : 'right';
+    this.animatedFrom = this.position;
+    if (this.position === 'left') {
+      this.overlayPositions = [{ originY: 'top', originX: 'start', overlayY: 'top', overlayX: 'start' }];
+      this.flexGrow = 0;
+      this.left = -parseFloat(parentStyle.paddingLeft);
+    } else { // We're right
+      this.overlayPositions = [{ originY: 'top', originX: 'end', overlayY: 'top', overlayX: 'end' }];
+      this.flexGrow = 1;
+      this.marginRight = -parseFloat(parentStyle.paddingRight);;
     }
-    ngAfterViewInit(): void {
-        const parentElement = this.el.nativeElement.parentElement;
-        const parentStyle = getComputedStyle(parentElement);
-        this.heightToolbar = parentStyle.height;
-        if (this.el.nativeElement.parentElement.childNodes[0] === this.el.nativeElement) { // We're left
-            this.overlayPositions = [{originY: 'top', originX: 'start', overlayY: 'top', overlayX: 'start'}];
-            this.flexGrow = 0;
-            this.left = -parseFloat(parentStyle.paddingLeft);
-        } else if (this.el.nativeElement.parentElement.childNodes[this.el.nativeElement.parentElement.childNodes.length - 1] === this.el.nativeElement) { // We're right
-            this.overlayPositions = [{ originY: 'top', originX: 'end', overlayY: 'top', overlayX: 'end' }];
-            this.marginRight = -parseFloat(parentStyle.paddingRight);;
-            this.flexGrow = 1;
+    if (this.animationDisabled) {
+      this.animatedFrom = null;
+    }
+    const matRowElement = this.el.nativeElement.closest('tr[mat-row], mat-row');
+    matRowElement.addEventListener('mouseenter', () => {
+      const parentStyle = getComputedStyle(parentElement);
+      this.heightToolbar = parentStyle.height;
+      this.open$.next(true);
+    }, { capture: true, once: false, passive: true });
+    matRowElement.addEventListener('mouseleave', (event: MouseEvent) => {
+      const newTarget = event.relatedTarget;
+      if (!this.hover$.getValue()) {
+        if (matRowElement.contains(newTarget)) {
+          this.open$.next(true);
+        } else {
+          this.open$.next(false);
         }
-    }
+      }
+    }, { capture: true, once: false, passive: true });
 
-    // We're right
-    @HostBinding('style.margin-right.px')
-    marginRight: number = 0;
-    @HostBinding('style.flex-grow')
-    flexGrow: number = 0;
+  }
 
-    // We're left
-    @HostBinding('style.left.px')
-    left: number = 0;
+  // We're right
+  @HostBinding('style.margin-right.px')
+  marginRight: number = 0;
+  @HostBinding('style.flex-grow')
+  flexGrow: number = 0;
+
+  // We're left
+  @HostBinding('style.left.px')
+  left: number = 0;
 
 
-    @Input()
-    color: string = 'primary';
-
-    @Input()
-    set open(open: boolean) {
-        this.open$.next(open);
-    }
+  @Input()
+  color: string = 'primary';
 }
